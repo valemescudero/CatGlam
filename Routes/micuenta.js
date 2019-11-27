849d1c76139149a2c57260bf49cdd38e442809d3
Evadne Meraki <evadne.meraki@gmail.com>
2019-11-27 20:25:02 -0300

    POST on usuarios now working

--- Routes/micuenta.js
+++ Routes/micuenta.js
@@ -16,53 +16,31 @@ router.get('/micuenta/registro',  (req,res) => {
    } );
 
 // Cliente --> servidor (formulario : method | action)
-router.post('/micuenta/registro', async(req,res,next)=> {
-   
-    console.log(req.body); // params
-    // /login/1 req.params
-
-    // req : Request (todo lo que llega al servidor)
-    //  params : contiene informacion de la URL
-    //  body : contiene informacion de parametros que se envian por POST
-        // --> {propiedad1 : nombre, propiedad2 : passwword}
-        // --> {usuario_u : 'franco', password_u : '1234'}
-    let usuario_u = req.body.usuario;
-    let password = req.body.password_u;
-    let data = await registro(usuario_u,password);
+router.post('/micuenta', async(req,res,next)=> {
     console.log(req.body);
-    if(data.length == 1) {
-        // permisos_u : nombre del campo
-        // [
-            // {nombre_u, permisos_u, mail_u}
-        //]
-        let permisos = data[0].permisos_u;
-        let id = data[0].id_u; // devuelve el id del usario  
-        console.log(permisos);
-        console.log(id)
-        if(permisos == 0) {
-            // no es admin
-            req.session.idUsuario = id;
-        } else {
-            // si es admin
-            // crear una variable de sesion de nombre : idAdmin
-            req.session.idAdmin = id;
+    let usuario = req.body.usuario;
+    let password = req.body.password;
+    let data = await ingreso(usuario,password);
+    console.log(req.body);
+ //   if(data.length == 1) {
+//        let permisos = data[0].permisos;
+//        let id = data[0].id; 
+//       console.log(permisos);
+//      console.log(id)
+//      if(permisos == 0) {
+//          req.session.idUsuario = id;
+//      } else {
+//          req.session.idAdmin = id;
+//      }
 
-        }
+        res.redirect('usuario');
 
-
-        // REQ.BODY.name
-        // REQ.PARAMS.
-        // el id es el identificador unico 
-       
-        res.redirect('/perfil');
-
-    } else {
-        res.render('registro', {message : 'Usuario o contraseña incorrectos'})
-    }
+//  } else {
+//      res.render('ingreso', {message : 'Usuario o contraseña incorrectos'})
+//  }
 
 });
-
-async function registro(usuario,password) {
+async function ingreso(usuario,password) {
     // select * from usuarios where usuario_u = usuario and password_u = password
 
     let query = "select * from usuarios where usuario_u = ? and password_u = ?";
@@ -71,5 +49,19 @@ async function registro(usuario,password) {
 
 
 }
+
+router.post('/micuenta/registro',async (req,res) => {
+    console.log("Control");
+    console.log(req.body);
+    let nuevoUsuario = {
+        nombre_u: req.body.nombre,
+        apellido_u: req.body.apellido,
+        usuario_u: req.body.usuario,
+        password_u: req.body.password,
+    }
+    await pool.query('insert into usuarios set ?', [nuevoUsuario]);
+    res.send('Received');
+})
+
 
 module.exports = router;
